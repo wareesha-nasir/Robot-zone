8915618684feff44720b5f44c1c6354df1b15d90
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-11-25 11:32:49 -0800

    Add files via upload

--- Robot-zone/src/Component/card/card.jsx
+++ Robot-zone/src/Component/card/card.jsx
@@ -4,8 +4,6 @@ import './card.styles.css';
 export const Card= props=>(
 <div className="card-container">
    <img alt="" src={`https://robohash.org/${props.movie.id}?set=set3&size=100x180`}/>
-
 <i><h2>
 {props.movie.name}
-      </h2></i> </div>
-);
+      </h2></i> </div>);
